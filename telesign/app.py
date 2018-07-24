b89cdea4c78f107218533323f1ed8d7c27b06eba
Vishket <vsshriwa@us.ibm.com>
2018-07-24 13:03:57 -0700

    added all endpoints

---
+++ telesign/app.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+
+"""
+RESTful service to manage collection of words
+
+Supported endpoints
+
+POST /words
+GET /words/<word>
+GET /words/count
+DELETE /words/<word>
+DELETE /words
+GET /palindromes/count
+GET /anagrams/count
+GET /anagrams/<word>
+
+"""
+
+# TODO: Test against body of text
+
+from flask import Flask, jsonify, request, abort
+from telesign import helper
+from telesign.database import Database
+
+
+app = Flask(__name__)
+
+
+@app.route('/v1/words/<word>', methods=['GET'])
+def get_word(word):
+    """
+    Function to return the word if it is present in the collection
+    :return: word str
+    """
+    dbconn = Database()
+    dbconn.select_all()
+    for record in dbconn.cursor:
+        if record[0] == word:
+            return str(word)
+    return abort(404)
+
+
+@app.route('/v1/words', methods=['POST', 'DELETE'])
+def words():
+    """
+    Function to support /words endpoint.
+    Adds words to collection if HTTP verb is POST
+    Deletes all words in collection of HTTP vers is DELETE
+    :return: bool
+    """
+    dbconn = Database()
+    collection = []
+
+    dbconn.select_all()
+    for record in dbconn.cursor:
+        collection.append(record[0])
+
+    if request.method == 'POST':
+        raw_data = request.get_json()
+        print raw_data
+        for word in set(raw_data):
+            if word not in collection:
+                word_size_check = helper.check_word_length(word)
+                word_letter_check = helper.check_word_letters(word)
+                if word_size_check and word_letter_check:
+                    dbconn.insert_data(word)
+        dbconn.select_all()
+        return jsonify(dbconn.cursor.fetchall())
+
+    if request.method == 'DELETE':
+        dbconn.delete_all_data()
+        dbconn.select_all()
+        return jsonify(dbconn.cursor.fetchall())
+
+
+@app.route('/v1/words/count', methods=['GET'])
+def get_words_count():
+    """
+    Function to return count of unique words in collection
+    :return: word_count str
+    """
+    dbconn = Database()
+
+    dbconn.select_all()
+    word_count = dbconn.cursor.rowcount
+    return str(word_count)
+
+
+@app.route('/v1/words/<word>', methods=['DELETE'])
+def delete_word(word):
+    """
+    Function to delete provided word from collection
+    :param word:
+    :return: updated_word_list: list
+    """
+    dbconn = Database()
+
+    dbconn.delete_data(word)
+    return str(True)
+
+
+@app.route('/v1/palindromes/count', methods=['GET'])
+def get_palindrome_count():
+    """
+    Function to return number of palindromic words in data store
+    :return: palindrome_count str
+    """
+    dbconn = Database()
+    palindrome_count = 0
+
+    dbconn.select_all()
+
+    for record in dbconn.cursor:
+        if helper.check_if_palindrome(record[0]):
+            palindrome_count += 1
+    return str(palindrome_count)
+
+
+@app.route('/v1/anagrams/<word>')
+def get_anagrams(word):
+    """
+    Function to check for anagrams of word provided
+    :return: anagram_list list
+    """
+    dbconn = Database()
+
+    anagram_list = []
+    collection = []
+
+    dbconn.select_all()
+    for record in dbconn.cursor:
+        collection.append(record[0])
+
+    if len(collection) > 0:
+        anagram_list = helper.get_anagram(word, collection)
+    return jsonify(anagram_list)
+
+
+@app.route('/v1/anagrams/count')
+def get_total_anagrams():
+    """
+    Function to return number of anagrams in data store
+    :return: anagram_count: str
+    """
+    dbconn = Database()
+
+    anagram_count = 0
+    collection = []
+
+    dbconn.select_all()
+    for record in dbconn.cursor:
+        collection.append(record[0])
+
+    if len(collection) > 0:
+        anagram_count = helper.get_total_anagram_count(collection)
+    return str(anagram_count)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True)
---
+++ telesign/helper.py
@@ -0,0 +1,75 @@
+"""
+A helper module to get anagram and palindrome of given strings
+"""
+
+#TODO: need to init? Static?
+
+
+def check_if_palindrome(word):
+    """
+    Function to check if given word is palindromic or not
+    :return: bool
+    """
+    return word == word[::-1]
+
+
+def are_anagram(word1, word2):
+    """
+    Function to check if pair of input words are anagram or not
+    :param word1: str
+    :param word2: str
+    :return: bool
+    """
+    return sorted(word1) == sorted(word2)
+
+
+def get_total_anagram_count(word_list=None):
+    """
+    Function to return the total number of anagrams in provided list
+    :param word_list: list of str
+    :return: int
+    """
+    seen = []
+    anagram_count = 0
+    # ["eat", "tea", "tan", "ate", "nat", "bat"]
+    # ["aet", "aet", "ant", "aet", "ant", "abt"]
+
+    for i in range(len(word_list)):
+        if ''.join(sorted(word_list[i])) not in seen:
+            j = i + 1
+            while j < len(word_list):
+                if sorted(word_list[i]) != sorted(word_list[j]):
+                    j += 1
+                else:
+                    anagram_count += 1
+                    seen.append(''.join(sorted(word_list[i])))
+                    break
+
+    return anagram_count
+
+
+def get_anagram(query_word, word_list):
+    """
+    Function to return list of words in word_list that are anagram of query_word
+    :param: query_word str word_list list
+    :return: anagram_list list
+    """
+    anagram_list = [x for x in word_list if sorted(query_word) == sorted(x)]
+    return anagram_list
+
+
+def check_word_length(word):
+    """
+    Function to check if length of word is less than 4
+    :return: bool
+    """
+    return len(word) < 4
+
+
+def check_word_letters(word):
+    """
+    Function to check of letters in the word are lower case ascii between a-z
+    :return: bool
+    """
+    return word.isalpha() and word.islower()
+
