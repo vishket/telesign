d4e6c377ec33eff22d25b9eea5cfb2867e57a9db
Vishket <vsshriwa@us.ibm.com>
2018-07-24 13:06:08 -0700

    Add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='telesign',
+    description='A simple microservice to maintain a collection of words.',
+    version='0.1.1',
+    author='VS',
+    packages=['telesign']
+)
+
+
